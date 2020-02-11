c42d553c201c641fa9f5d929db30d1d20f76624c
paulangcrackers <bmariepaula121@gmail.com>
2020-02-11 14:24:59 +0800

    Updates

---
+++ precache-manifest.126c62819cd198be1ba69cd22d659480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a74f9d2d95b78dedf7986cf9698b1c",
+    "url": "/myCRUDAPP/index.html"
+  },
+  {
+    "revision": "c0483a05e4e7bc6711a9",
+    "url": "/myCRUDAPP/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "742aea35f19e447465fc",
+    "url": "/myCRUDAPP/static/css/main.68ff730a.chunk.css"
+  },
+  {
+    "revision": "c0483a05e4e7bc6711a9",
+    "url": "/myCRUDAPP/static/js/2.942bc7ea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myCRUDAPP/static/js/2.942bc7ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742aea35f19e447465fc",
+    "url": "/myCRUDAPP/static/js/main.07649105.chunk.js"
+  },
+  {
+    "revision": "0220ee39ed8fca4f65c1",
+    "url": "/myCRUDAPP/static/js/runtime-main.fe591166.js"
+  }
+]);
